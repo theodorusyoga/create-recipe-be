cee83a849294b45fbdac4ad9edb73d5f325399d1
Theodorus Yoga <theodorus.yoga@pinhome.id>
2023-03-30 19:42:47 +0700

    Finalizing all endpoints for completions and images. Added auth middleware

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { getCompletionChat } from "./routes";
+import { getCompletionChat, getImages } from "./routes";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 var app = express();
+app.use(cors());
+app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use("/", getCompletionChat);
+app.use("/chats", getCompletionChat, getImages);
 
 app.listen(3001, () => {
   console.log(`Started on port 3001`);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,63 @@
 import { Router } from "express";
+import axios from "axios";
 
 const router = Router();
+const API_KEY = "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";
+const STATIC_API_KEY = "bd7a997a-8762-47fb-92de-b3b88bf4a0ea";
 
-export const getCompletionChat = router.get("/completion", (req, res) => {
-  return res.status(200).send({ hello: "hello" });
-});
+export const authMiddleware = (req, res, next) => {
+  const token = req.headers["authorization"];
+  const cleanToken = token?.replace("Bearer ", "");
+
+  if (cleanToken === STATIC_API_KEY) {
+    next();
+  } else {
+    return res.status(401).send({
+      message: "Unauthorized",
+    });
+  }
+};
+
+export const getCompletionChat = router.post(
+  "/completions",
+  authMiddleware,
+  async (req, res) => {
+    try {
+      const response = await axios.post(
+        "https://api.openai.com/v1/chat/completions",
+        req.body,
+        {
+          headers: {
+            Authorization: `Bearer ${API_KEY}`,
+          },
+        }
+      );
+      const { data } = response;
+      return res.status(200).send(data);
+    } catch (e) {
+      return res.status(500).send(e);
+    }
+  }
+);
+
+export const getImages = router.post(
+  "/images",
+  authMiddleware,
+  async (req, res) => {
+    try {
+      const response = await axios.post(
+        "https://api.openai.com/v1/images/generations",
+        req.body,
+        {
+          headers: {
+            Authorization: `Bearer ${API_KEY}`,
+          },
+        }
+      );
+      const { data } = response;
+      return res.status(200).send(data);
+    } catch (e) {
+      return res.status(500).send(e);
+    }
+  }
+);
