227e08f7f51928e1ce675648f0db57aaa5d7421f
Theodorus Yoga <theodorus.yoga@pinhome.id>
2023-03-31 08:43:59 +0700

    Add validator and change request body payload

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import axios from "axios";
+import { body, validationResult } from "express-validator";
 
 const router = Router();
 
@@ -19,11 +20,27 @@ export const authMiddleware = (req, res, next) => {
 export const getCompletionChat = router.post(
   "/completions",
   authMiddleware,
+  body("ingredients").isArray().withMessage("Ingredients must be an array"),
   async (req, res) => {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const { ingredients } = req.body;
+      const ingredientStr = ingredients.join(", ");
       const response = await axios.post(
         "https://api.openai.com/v1/chat/completions",
-        req.body,
+        {
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "system",
+              content: `give me a recipe that I can make using: ${ingredientStr}`,
+            },
+          ],
+        },
         {
           headers: {
             Authorization: `Bearer ${process.env.CHATGPT_API_KEY}`,
@@ -33,6 +50,7 @@ export const getCompletionChat = router.post(
       const { data } = response;
       return res.status(200).send(data);
     } catch (e) {
+      console.log("Error", e);
       return res.status(500).send(e);
     }
   }
@@ -41,11 +59,22 @@ export const getCompletionChat = router.post(
 export const getImages = router.post(
   "/images",
   authMiddleware,
+  body("prompt").isString().withMessage("Prompt must be a string"),
   async (req, res) => {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const { prompt } = req.body;
       const response = await axios.post(
         "https://api.openai.com/v1/images/generations",
-        req.body,
+        {
+          prompt,
+          n: 2,
+          size: "512x512",
+        },
         {
           headers: {
             Authorization: `Bearer ${process.env.CHATGPT_API_KEY}`,
